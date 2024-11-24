e6f67f52e3ed81335ea941c687884e1477e49d3c
evanb5 <evanbai2004@gmail.com>
2024-11-24 16:08:19 -0500

    added BuildResumeInteractorTest.java

---
+++ src/test/java/use_case/build_resume/BuildResumeInteractorTest.java
@@ -0,0 +1,18 @@
+package use_case.build_resume;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
+import org.junit.jupiter.api.Test;
+import services.ChatGPTService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildResumeInteractorTest {
+
+    @Test
+    void successGenerateResumeTest() {
+
+    }
+}
