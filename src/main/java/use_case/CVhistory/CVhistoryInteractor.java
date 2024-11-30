221dbfa67e60292ee6219f3cbe7893ff37574935
BobbyYsq <bobbyysq@gmail.com>
2024-11-30 07:12:21 -0500

    Modify If statements in CVhistoryInteractor.java

--- src/main/java/use_case/CVhistory/CVhistoryInteractor.java
+++ src/main/java/use_case/CVhistory/CVhistoryInteractor.java
@@ -20,41 +20,39 @@ public class CVhistoryInteractor implements CVhistoryInputBoundary{
     public void CVhistory(CVhistoryInputData inputData) {
         User user = userDataAccess.getCurrentUser();
         int index = inputData.getIndex();
-        if(user != null) {
-            if (index >= 0 && index  <= user.getCvs().size()) {
-                List<String> keys = new ArrayList<>(user.getCvs().keySet());
+        if (user != null) {
+            List<String> keys = new ArrayList<>(user.getCvs().keySet());
+            if (index >= 0 && index < keys.size()) {
                 CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(index)), keys);
                 user.setCVindex(index);
                 this.presenter.present(outputData);
-            }else if(index == -4) {
-                List<String> keys = new ArrayList<>(user.getCvs().keySet());
+            } else if (index == -4) {
                 CVhistoryOutputData outputData = new CVhistoryOutputData("", keys);
                 this.presenter.present(outputData);
                 user.setCVindex(-4);
-            }else if(index == -2 && user.getCVindex()>0) {
-                List<String> keys = new ArrayList<>(user.getCvs().keySet());
-                CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(user.getCVindex() - 1)), keys);
-                user.setCVindex(user.getCVindex() - 1);
+            } else if (index == -2 && user.getCVindex() > 0) {
+                int newIndex = user.getCVindex() - 1;
+                CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(newIndex)), keys);
+                user.setCVindex(newIndex);
                 this.presenter.present(outputData);
-            }else if(index == -3 && user.getCVindex()<user.getCvs().size()-1 && user.getCVindex()>=0) {
-                List<String> keys = new ArrayList<>(user.getCvs().keySet());
-                CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(user.getCVindex() + 1)), keys);
-                user.setCVindex(user.getCVindex() + 1);
+            } else if (index == -3 && user.getCVindex() >= 0 && user.getCVindex() < keys.size() - 1) {
+                int newIndex = user.getCVindex() + 1;
+                CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(newIndex)), keys);
+                user.setCVindex(newIndex);
                 this.presenter.present(outputData);
-            }else{
-                if (user.getCVindex() != -4){
-                    List<String> keys = new ArrayList<>(user.getCvs().keySet());
+            } else {
+                if (user.getCVindex() != -4 && user.getCVindex() >= 0 && user.getCVindex() < keys.size()) {
                     CVhistoryOutputData outputData = new CVhistoryOutputData(user.getCvs().get(keys.get(user.getCVindex())), keys);
                     this.presenter.present(outputData);
-                }else {
-                    List<String> keys = new ArrayList<>(user.getCvs().keySet());
+                } else {
                     CVhistoryOutputData outputData = new CVhistoryOutputData("", keys);
                     this.presenter.present(outputData);
                 }
             }
-        }else {
-            CVhistoryOutputData outputData = new CVhistoryOutputData("",new ArrayList<>());
+        } else {
+            CVhistoryOutputData outputData = new CVhistoryOutputData("", new ArrayList<>());
             this.presenter.present(outputData);
         }
+    }
 
-}}
+}
